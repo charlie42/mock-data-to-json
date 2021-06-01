6c6033cb0dd00d255c7818d6d60322d785ab2cde
charlie42 <oninfrared@gmail.com>
2021-06-01 15:24:30 +0200

    group domains by categories

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
+// npm i -s csv-parser
+
 const util = require('util')
-const obj = require('./output.json')
-console.log(obj)
+const csv = require('csv-parser');
+const fs = require('fs');
+let obj = require('./output.json')
 
-obj["students"].forEach(student => {
+function groupByTeacher(obj) {
+    obj["students"].forEach(student => {
     student["dates"].forEach(date => {
-
         date = Object.values(
             date["domains"].reduce((a, v) => {
-                
                 if (!a[v.domain]) {
                     a[v.domain] = {
                         domain_name: v.domain,
@@ -15,24 +17,75 @@ obj["students"].forEach(student => {
                         teachers: v.teachers
                     }
                 }
-
                 Object.entries(v.teachers).forEach(([key, value]) => {
                     if (!a[v.domain].entry_level_scores) {
                         a[v.domain].entry_level_scores = []
                     }
                     a[v.domain].entry_level_scores.push(value["entry-level-score"])
                 })
-                
                 return a
             }, {})
-        )
+        )        
+        })
+    });
+    return obj
+}
 
-        console.log(util.inspect(date, false, null, true /* enable colors */))
-
-        // date["domains"].forEach(domain => {
-        //     console.log(domain)
-            
-        // })
-        
+function groupByCategory(obj, categoryDictList) {
+    obj["students"].forEach(student => {
+        student["dates"].forEach(date => {
+            date["categories"] = []
+            categorySet = new Set(getValuesFromDictList(categoryDictList))
+            categorySet.forEach(category => {
+                domainsInCategory = getDomainsForCategory(category, categoryDictList)
+                domainObjectsInCategory = []
+                date["domains"].forEach(domain => {
+                    if (domainsInCategory.includes(domain.domain)) {
+                        domainObjectsInCategory.push(domain)
+                    }
+                })
+                date["categories"].push({
+                    category: category,
+                    domains: domainObjectsInCategory
+                })
+            })
+            delete date.domains;
+        })
     })
-});
+    return obj
+}
+
+function getValuesFromDictList(categoryDictList) {
+    return categoryDictList.map(function(obj) {
+        return obj.category;
+    });
+}
+
+function getDomainsForCategory(category, categoryDictList) {
+    domains = []
+    categoryDictList.map(function(obj) {
+        if (obj.category == category) {
+            domains.push(obj.domain);
+        }
+    });
+    return domains
+}
+
+obj = groupByTeacher(obj)
+//console.log(util.inspect(obj, false, null, true /* enable colors */))
+
+categoriesFilePath = "categories.csv"
+let categoryDictList = []
+fs.createReadStream(categoriesFilePath)
+    .pipe(csv())
+    .on('data', (r) => {
+        categoryDictList.push(r);        
+    })
+    .on('end', () => {
+        //console.log("categoryDict", categoryDictList)
+        obj = groupByCategory(obj, categoryDictList)
+        console.log(util.inspect(obj, false, null, true /* enable colors */))
+
+    });
+
+
