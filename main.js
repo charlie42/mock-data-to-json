8863c91291c49cc1886ef8383f593ad17808d09b
charlie42 <oninfrared@gmail.com>
2021-06-02 15:19:51 +0200

    agg by category

--- main.js
+++ main.js
@@ -3,31 +3,48 @@
 const util = require('util')
 const csv = require('csv-parser');
 const fs = require('fs');
-let obj = require('./output.json')
 
 function groupByTeacher(obj) {
+
     obj["students"].forEach(student => {
-    student["dates"].forEach(date => {
-        date = Object.values(
-            date["domains"].reduce((a, v) => {
-                if (!a[v.domain]) {
-                    a[v.domain] = {
-                        domain_name: v.domain,
-                        entry_level_scores: [],
-                        teachers: v.teachers
+        let studentDateResult = student["dates"].map(date => {
+            dateResult = date
+
+            //add every teacher entry level score to an array
+            dateResult["domains"] = Object.values(
+                date["domains"].reduce((a, v) => {
+                    if (!a[v.domain]) {
+                        a[v.domain] = {
+                            domain: v.domain,
+                            entry_level_scores: [],
+                            teachers: v.teachers
+                        }
                     }
-                }
-                Object.entries(v.teachers).forEach(([key, value]) => {
-                    if (!a[v.domain].entry_level_scores) {
-                        a[v.domain].entry_level_scores = []
-                    }
-                    a[v.domain].entry_level_scores.push(value["entry-level-score"])
-                })
-                return a
-            }, {})
-        )        
+                    Object.entries(v.teachers).forEach(([key, value]) => {
+                        if (!a[v.domain].entry_level_scores) {
+                            a[v.domain].entry_level_scores = []
+                        }
+                        a[v.domain].entry_level_scores.push(parseFloat(value["entry-level-score"]))
+                    })
+                    return a
+                }, {})
+            )
+
+            //get avg min and max scores from the array
+            dateResult["domains"].forEach(domain => {
+                domain.avg_entry_level_score = getAvgOfArray(domain.entry_level_scores)
+                domain.min_entry_level_score = Math.min(...domain.entry_level_scores)
+                domain.max_entry_level_score = Math.max(...domain.entry_level_scores)
+            })
+
+            return dateResult  
         })
+        student["dates"] = studentDateResult
+        // console.log(util.inspect(student, false, null, true /* enable colors */))  
+        //console.log(util.inspect(studentDateResult, false, null, true /* enable colors */))  
     });
+    
+    //console.log(util.inspect(obj, false, null, true /* enable colors */))  
     return obj
 }
 
@@ -55,12 +72,47 @@ function groupByCategory(obj, categoryDictList) {
     return obj
 }
 
-function aggregateScoresByCategory(obj) {
+function aggregateScoredByCategory(obj) {
+    obj.students.forEach(student => {
+        let studentDateResult = student.dates.map(date => {
+            dateResult = date
+
+            //add every teacher entry level score to an array
+            dateResult.categories = Object.values(
+                date.categories.reduce((a, v) => {
+                    if (!a[v.category]) {
+                        a[v.category] = {
+                            category: v.category,
+                            entry_level_scores: [],
+                            domains: v.domains
+                        }
+                    }
+                    Object.entries(v.domains).forEach(([key, value]) => {
+                        if (!a[v.category].entry_level_scores) {
+                            a[v.category].entry_level_scores = []
+                        }
+                        a[v.category].entry_level_scores.push(parseFloat(value.avg_entry_level_score))
+                    })
+                    return a
+                }, {})
+            )
+
+            //get avg min and max scores from the array
+            dateResult.categories.forEach(category => {
+                category.avg_entry_level_score = getAvgOfArray(category.entry_level_scores)
+                category.min_entry_level_score = Math.min(...category.entry_level_scores)
+                category.max_entry_level_score = Math.max(...category.entry_level_scores)
+            })
+
+            return dateResult  
+        })
+        student.dates = studentDateResult
+    })
     return obj
 }
 
 function writeToFile(obj, outputFilePath) {
-    var json = JSON.stringify(obj);
+    var json = JSON.stringify(obj, null, 2);
     fs.writeFileSync(outputFilePath, json, 'utf8');
 }
 
@@ -81,11 +133,20 @@ function getDomainsForCategory(category, categoryDictList) {
     return domains
 }
 
+function getAvgOfArray(arr) {
+    return arr.reduce((a,b) => parseFloat(a) + parseFloat(b), 0) / arr.length
+}
+
+let obj = require('./output.json')
+
+//console.log(util.inspect(obj, false, null, true /* enable colors */))
 obj = groupByTeacher(obj)
 //console.log(util.inspect(obj, false, null, true /* enable colors */))
 
+
 categoriesFilePath = "categories.csv"
 outputFilePath = "aggregated.json"
+
 let categoryDictList = []
 fs.createReadStream(categoriesFilePath)
     .pipe(csv())
@@ -95,9 +156,10 @@ fs.createReadStream(categoriesFilePath)
     .on('end', () => {
         //console.log("categoryDict", categoryDictList)
         obj = groupByCategory(obj, categoryDictList)
-        console.log(util.inspect(obj, false, null, true /* enable colors */))
+        
 
-        obj = aggregateScoresByCategory(obj)
+        obj = aggregateScoredByCategory(obj)
+        console.log(util.inspect(obj, false, null, true /* enable colors */))
 
         writeToFile(obj, outputFilePath)
 
